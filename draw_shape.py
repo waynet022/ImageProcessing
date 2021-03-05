20f0960d5398ca7098278078105b710b36f92407
wayne1tam <waynevick7226@hotmail.com>
2021-03-05 18:30:36 -0500

    Draw shapes

--- draw_shape.py
+++ draw_shape.py
@@ -1,15 +1,34 @@
 import cv2 as cv
 import numpy as np
 
-blank = np.zeros((500,500,3), dtype='uint8')
+rect_blank = np.zeros((500,500,3), dtype='uint8')
 
-cv.rectangle(blank, (10,10), (250,500), (0,255,0), thickness=2)
-cv.imshow('Rectangle', blank)
+cv.rectangle(rect_blank, (10,10), (250,500), (0,255,0), thickness=2)
+cv.imshow('Rectangle', rect_blank)
 
+fill_blank = np.zeros((500,500,3), dtype='uint8')
 # to fill the rectangle, thickness = cv.FILLED or -1
-cv.rectangle(blank, (10,10), (250,500), (0,255,0), thickness=cv.FILLED)
-cv.imshow('Filled Rectangle', blank)
+cv.rectangle(fill_blank, (10,10), (250,500), (0,255,0), thickness=cv.FILLED)
+cv.imshow('Filled Rectangle', fill_blank)
+
+shape_blank = np.zeros((500,500,3), dtype='uint8')
 # instead of using coordinates, can also use shape of image
-cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (255,255,0), thickness=cv.FILLED)
-cv.imshow('Filled Rectangle using image size', blank)
+cv.rectangle(shape_blank, (0,0), (shape_blank.shape[1]//2, shape_blank.shape[0]//2), (255,255,0), thickness=cv.FILLED)
+cv.imshow('Filled Rectangle using image size', shape_blank)
+
+circle_blank = np.zeros((500,500,3), dtype='uint8')
+# circle
+cv.circle(circle_blank, (circle_blank.shape[1]//2, circle_blank.shape[0]//2), 40, (0,255,0), thickness=3)
+cv.imshow('Circle', circle_blank)
+
+line_blank = np.zeros((500,500,3), dtype='uint8')
+# line
+cv.line(line_blank, (0,0), (line_blank.shape[1]//2, line_blank.shape[0]//2), (255,255,255), thickness=3)
+cv.imshow('Line', line_blank)
+
+text_blank = np.zeros((500,500,3), dtype='uint8')
+# write text on image
+cv.putText(text_blank, 'hello', (200,255), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0),2)
+cv.imshow('Text on image', text_blank)
+
 cv.waitKey(0)
