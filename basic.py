f6254a05ef34c5bb363a165ec68a9a3f575c761c
wayne1tam <waynevick7226@hotmail.com>
2021-03-05 21:04:49 -0500

    Add erode function

--- basic.py
+++ basic.py
@@ -17,8 +17,8 @@ def blur_image(image, mask, show=False):
         if show:
             cv.imshow('Blurred Image', output_image)
             cv.waitKey(0)
-        else:
-            return output_image
+        
+        return output_image
     except ValueError:
         exit('Mask size must be uneven!')
 
@@ -28,26 +28,34 @@ def cascade_image(image, th1, th2, show):
     if show:
         cv.imshow('Canny Edges', output_image)
         cv.waitKey(0)
-    else:
-        return output_image
+    
+    return output_image
 
 def dilate_image(image, kernel_size, iterations=1, show=False):
     output_image = cv.dilate(image, (kernel_size, kernel_size), iterations=iterations)
     if show:
         cv.imshow('Dilated Image', output_image)
         cv.waitKey(0)
-    else:
-        return output_image
+    
+    return output_image
 
+def erode_image(image, kernel_size, iterations=1, show=False):
+    output_image = cv.erode(image, (kernel_size, kernel_size), iterations=iterations)
+    if show:
+        cv.imshow('Eroded image', output_image)
+        cv.waitKey(0)
+    
+    return output_image
 
 if __name__=='__main__':
     
     image_file = 'assets/images/park.jpg'
     img = cv.imread(image_file)
     
-    blur_img = blur_image(img, 7, show=False)
-    cascade = cascade_image(blur_img, 125, 175, show=False)
-    dilate_image(cascade, 7, show=True)
+    blur_img = blur_image(img, 7, False)
+    cascade = cascade_image(blur_img, 125, 175, False)
+    dl = dilate_image(cascade, 7)
+    erode_image(dl, 3, show=True)
 
 
     
