8184b4704e1beada3adb0200a169e177470fce5b
wayne1tam <waynevick7226@hotmail.com>
2021-03-05 21:29:44 -0500

    Add resize and crop image

--- basic.py
+++ basic.py
@@ -23,7 +23,7 @@ def blur_image(image, mask, show=False):
         exit('Mask size must be uneven!')
 
 # use as default th1=125, th2=175
-def cascade_image(image, th1, th2, show):
+def cascade_image(image, th1, th2, show=False):
     output_image = cv.Canny(image, th1, th2)
     if show:
         cv.imshow('Canny Edges', output_image)
@@ -47,15 +47,37 @@ def erode_image(image, kernel_size, iterations=1, show=False):
     
     return output_image
 
+def resize_image(image, height, width, show=False):
+    '''
+        interpolations options are
+        cv.INTER_AREA for shrinking images to dimensions that are smaller than the original dimensions
+        cv.INTER_LINEAR for enlarging and scaling image to larger dimensions
+        cv.INTER_CUBIC same as above, but higher quality image at the cost of speed (so slower than above)
+    '''
+    
+    output_image = cv.resize(image, (height, width), interpolation=cv.INTER_CUBIC)
+    if show:
+        cv.imshow(f'Resized to {height} x {width}', output_image)
+        cv.waitKey(0)
+    return output_image
+
+def crop_image(image, x1, x2, y1, y2, show=False):
+    output_image = image[x1:x2, y1:y2]
+    if show:
+        cv.imshow('Cropped Image', output_image)
+        cv.waitKey(0)
+    return output_image
+
 if __name__=='__main__':
     
     image_file = 'assets/images/park.jpg'
     img = cv.imread(image_file)
     
-    blur_img = blur_image(img, 7, False)
-    cascade = cascade_image(blur_img, 125, 175, False)
+    blur_img = blur_image(img, 7)
+    cascade = cascade_image(blur_img, 125, 175)
     dl = dilate_image(cascade, 7)
-    erode_image(dl, 3, show=True)
+    ei = erode_image(dl, 3)
+    resize_image(ei, 500, 500, True)
 
 
     
