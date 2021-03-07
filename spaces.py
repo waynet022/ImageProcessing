b332a95a23f7da64e6282c068afaece9065c8ee6
wayne1tam <waynevick7226@hotmail.com>
2021-03-07 01:58:38 -0500

    Convert hsv to bgr function

--- spaces.py
+++ spaces.py
@@ -35,11 +35,17 @@ def convert_rgb(image, show=False):
         cv.waitKey(0)
     return output
 
+def hsv_convert_bgr(image, show=False):
+    output = cv.cvtColor(image, cv.COLOR_HSV2BGR)
+    if show:
+        cv.imshow('HSV to BGR', output)
+        cv.waitKey(0)
+    return output
+
 if __name__=='__main__':
     image_file = 'assets/images/park.jpg'
     img = cv.imread(image_file)
-    convert_gray(img, show=True)
-    convert_hsv(img, show=True)
-    convert_lab(img, show=True)
-    rgb_image = convert_rgb(img, show=True)
-    show_with_mat(rgb_image)
+    hsv = convert_hsv(img, show=True)
+    hsv_convert_bgr(hsv, show=True)
+    
+    
