3c2eac1b0d8d463c4da037b69aab3cb1d98df246
wayne1tam <waynevick7226@hotmail.com>
2021-03-07 01:45:29 -0500

    Use matplotlib to display image

--- spaces.py
+++ spaces.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 def convert_gray(image, show=False):
     output = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
@@ -22,9 +23,14 @@ def convert_lab(image, show=False):
         cv.imshow('LAB Image', output)
         cv.waitKey(0)
 
+def show_with_mat(image):
+    plt.imshow(image)
+    plt.show()
+
 if __name__=='__main__':
     image_file = 'assets/images/park.jpg'
     img = cv.imread(image_file)
     convert_gray(img, show=True)
     convert_hsv(img, show=True)
     convert_lab(img, show=True)
+    show_with_mat(img)
