971bb693b33c70b057dac1237709c6459c2dcb75
wayne1tam <waynevick7226@hotmail.com>
2021-03-08 03:56:28 -0500

    Add median blurring

--- smoothing.py
+++ smoothing.py
@@ -22,9 +22,17 @@ def gaussian_blur(image, kernel, sigma=0, show=False):
         cv.waitKey(0)
     return output
 
+def median_blur(image, kernel, show=True):
+    output = cv.medianBlur(image, kernel)
+    if show:
+        cv.imshow('Median Blur', output)
+        cv.waitKey(0)
+    return output
+
 if __name__=='__main__':
     image_file = 'assets/images/group 1.jpg'
     img = cv.imread(image_file)
     cv.imshow('Original Image', img)
     average_blur(img, 3, show=True)
     gaussian_blur(img, 3, 0, show=True)
+    median_blur(img, 3, show=True)
