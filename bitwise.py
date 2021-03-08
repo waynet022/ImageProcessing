690701b6eabe052f5dd70997817371af8b5e82fa
wayne1tam <waynevick7226@hotmail.com>
2021-03-08 17:22:09 -0500

    Create basic shapes and add bitwise combinations

---
+++ bitwise.py
@@ -0,0 +1,37 @@
+import cv2 as cv
+import numpy as np
+
+def create_rectangle(image,start, end, color=255, thickness=-1, show=False):
+    output = cv.rectangle(image.copy(), start, end, color, thickness)
+    if show:
+        cv.imshow('Rectangle', output)
+        cv.waitKey(0)
+    return output
+
+def create_circle(image, center, radius, color=255, thickness=-1, show=False):
+    output = cv.circle(image.copy(), center, radius, 255, -1)
+    if show:
+        cv.imshow('Circle', output)
+        cv.waitKey(0)
+    return output
+
+def bitwise_and(image_1, image_2, show=False):
+    output = cv.bitwise_and(image_1, image_2)
+    if show:
+        cv.imshow('Bitwise And Image', output)
+        cv.waitKey(0)
+    return output
+
+def bitwise_or(image_1, image_2, show=False):
+    output = cv.bitwise_or(image_1, image_2)
+    if show:
+        cv.imshow('Bitwise Or Image', output)
+        cv.waitKey(0)
+    return output
+
+if __name__=='__main__':
+    blank = np.zeros((400,400), dtype='uint8')
+    rectangle = create_rectangle(blank, (30,30), (370,370))
+    circle = create_circle(blank, (200,200), 200)
+    bitwise_and(rectangle, circle, show=True)
+    bitwise_or(rectangle, circle, show=True)
