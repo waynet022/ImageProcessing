458e26fd6ff7d298715687e2a39aaec042d9a3f2
wayne1tam <waynevick7226@hotmail.com>
2021-03-08 03:52:32 -0500

    Add gaussian blurring

--- smoothing.py
+++ smoothing.py
@@ -10,8 +10,21 @@ def average_blur(image, kernel, show=False):
         cv.waitKey(0)
     return output
 
+def gaussian_blur(image, kernel, sigma=0, show=False):
+    '''
+    each surrounding pixel has a weight value. The average of the product
+    of the weights give the value of the true center. The gaussian method has 
+    less blurring than the averaging method, the gaussian blur is more natural.
+    '''
+    output = cv.GaussianBlur(image, (kernel, kernel), sigma)
+    if show:
+        cv.imshow('Gaussian Blur', output)
+        cv.waitKey(0)
+    return output
+
 if __name__=='__main__':
     image_file = 'assets/images/group 1.jpg'
     img = cv.imread(image_file)
     cv.imshow('Original Image', img)
     average_blur(img, 3, show=True)
+    gaussian_blur(img, 3, 0, show=True)
