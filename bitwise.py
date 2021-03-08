92f941b9fa04acceb1d1ea05cb24b71f231f1570
wayne1tam <waynevick7226@hotmail.com>
2021-03-08 17:27:43 -0500

    Add xor and not

--- bitwise.py
+++ bitwise.py
@@ -29,9 +29,25 @@ def bitwise_or(image_1, image_2, show=False):
         cv.waitKey(0)
     return output
 
+def bitwise_xor(image_1, image_2, show=False):
+    output = cv.bitwise_xor(image_1, image_2)
+    if show:
+        cv.imshow('Bitwise xor Image', output)
+        cv.waitKey(0)
+    return output
+
+def bitwise_not(image, show=False):
+    output = cv.bitwise_not(image)
+    if show:
+        cv.imshow('Bitwise not Image', output)
+        cv.waitKey(0)
+    return output
+
 if __name__=='__main__':
     blank = np.zeros((400,400), dtype='uint8')
     rectangle = create_rectangle(blank, (30,30), (370,370))
     circle = create_circle(blank, (200,200), 200)
     bitwise_and(rectangle, circle, show=True)
     bitwise_or(rectangle, circle, show=True)
+    bitwise_xor(rectangle, circle, show=True)
+    bitwise_not(circle, show=True)
