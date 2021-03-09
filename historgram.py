d1f0ae70d90727d034f0911f59c5c09b52aaa42c
wayne1tam <waynevick7226@hotmail.com>
2021-03-08 23:55:23 -0500

    Use display option

--- historgram.py
+++ historgram.py
@@ -5,16 +5,19 @@ from spaces import convert_gray
 def histogram(image, show=False):
     hist = cv.calcHist([image], [0], None, [256], [0,256])
 
-    plt.figure()
-    plt.title('Grayscale Histogram')
-    plt.xlabel('Bins')
-    plt.ylabel('Number of pixels')
-    plt.plot(hist)
-    plt.xlim([0,256])
-    plt.show()
+    if show:
+        plt.figure()
+        plt.title('Grayscale Histogram')
+        plt.xlabel('Bins')
+        plt.ylabel('Number of pixels')
+        plt.plot(hist)
+        plt.xlim([0,256])
+        plt.show()
+
+    return hist
 
 if __name__=='__main__':
     image_file = 'assets/images/cats.jpg'
     img = cv.imread(image_file)
     gray = convert_gray(img)
-    histogram(img)
+    histogram(img, show=True)
