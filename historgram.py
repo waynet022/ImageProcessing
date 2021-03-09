2ca027cbd10f376b370cd55f92b272f809391288
wayne1tam <waynevick7226@hotmail.com>
2021-03-08 23:54:04 -0500

    Create histogram function

---
+++ historgram.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+from spaces import convert_gray
+
+def histogram(image, show=False):
+    hist = cv.calcHist([image], [0], None, [256], [0,256])
+
+    plt.figure()
+    plt.title('Grayscale Histogram')
+    plt.xlabel('Bins')
+    plt.ylabel('Number of pixels')
+    plt.plot(hist)
+    plt.xlim([0,256])
+    plt.show()
+
+if __name__=='__main__':
+    image_file = 'assets/images/cats.jpg'
+    img = cv.imread(image_file)
+    gray = convert_gray(img)
+    histogram(img)
