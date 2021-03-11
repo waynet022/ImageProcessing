fd9e5f5648bcdb1bea6a5ad77111746de56ba528
wayne1tam <waynevick7226@hotmail.com>
2021-03-11 03:19:16 -0500

    Add arguments for displaying image with color spaces

--- reader.py
+++ reader.py
@@ -1,13 +1,21 @@
 import cv2 as cv
 import argparse
+from spaces import color_space_convert
 
 def parser():
     parser = argparse.ArgumentParser(description='OpenCV Reader')
     parser.add_argument("--image", type=str, help='Path to the image')
-    parser.add_argument("--video", type=str, help='Path to the video')
+    parser.add_argument("--space", type=str, help='Color space to convert to [hsv, lab, gray, rgb]')
     parser.add_argument("--scale", type=float, default=1.00, help='Scale size of output')
+    parser.add_argument("--show", type=bool, default=False, help='Show output')
     parser.add_argument("--output", type=str, default='output/output_image.jpg', help='Output location of file')
 
     return parser.parse_args()
 
-    
+if __name__=='__main__':
+    args = parser()
+    image_path = args.image
+    image=cv.imread(image_path)
+    conversion=args.space
+    output=args.output
+    color_space_convert(image, conversion, show=args.show)
