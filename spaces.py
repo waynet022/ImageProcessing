2c902fb3d8b7e4a00f30b824896f4f4cb99b8e09
wayne1tam <waynevick7226@hotmail.com>
2021-03-11 03:27:07 -0500

    Remove old single conversion functions

--- spaces.py
+++ spaces.py
@@ -17,39 +17,9 @@ def color_space_convert(image, conversion, show=False):
         cv.waitKey(0)
     return output
 
-def convert_gray(image, show=False):
-    output = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    if show:
-        cv.imshow('Gray Image', output)
-        cv.waitKey(0)
-    
-    return output
-
-def convert_hsv(image, show=False):
-    output = cv.cvtColor(image, cv.COLOR_BGR2HSV)
-    if show:
-        cv.imshow('HSV Image', output)
-        cv.waitKey(0)
-
-    return output
-
-def convert_lab(image, show=False):
-    output = cv.cvtColor(image, cv.COLOR_BGR2LAB)
-    if show:
-        cv.imshow('LAB Image', output)
-        cv.waitKey(0)
-    return output
-
 def show_with_mat(image):
     plt.imshow(image)
     plt.show()
-
-def convert_rgb(image, show=False):
-    output = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-    if show:
-        cv.imshow('RGB Image', output)
-        cv.waitKey(0)
-    return output
 
 def hsv_convert_bgr(image, show=False):
     output = cv.cvtColor(image, cv.COLOR_HSV2BGR)
@@ -68,6 +38,6 @@ def lab_convert_bgr(image, show=False):
 if __name__=='__main__':
     image_file = 'assets/images/park.jpg'
     img = cv.imread(image_file)
-    lab = convert_lab(img, show=True)
+    lab = color_space_convert(img, 'lab', show=True)
     lab_convert_bgr(lab, show=True)
     
