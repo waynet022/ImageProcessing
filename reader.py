8deb1c03c0cc118e3e978771cbe798ed34b22a83
wayne1tam <waynevick7226@hotmail.com>
2021-03-11 03:01:24 -0500

    Add output path to arguments

--- reader.py
+++ reader.py
@@ -6,6 +6,7 @@ def parser():
     parser.add_argument("--image", type=str, help='Path to the image')
     parser.add_argument("--video", type=str, help='Path to the video')
     parser.add_argument("--scale", type=float, default=1.00, help='Scale size of output')
+    parser.add_argument("--output", type=str, default='output/output_image.jpg', help='Output location of file')
 
     return parser.parse_args()
 
