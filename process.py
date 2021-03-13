cf888ee50cb39b7bbc273d9f9103b4c52028de74
wayne1tam <waynevick7226@hotmail.com>
2021-03-12 23:25:20 -0500

    Add original as default argument

--- process.py
+++ process.py
@@ -5,7 +5,7 @@ from spaces import color_space_convert
 def parser():
     parser = argparse.ArgumentParser(description='OpenCV Reader')
     parser.add_argument("--image", type=str, help='Path to the image')
-    parser.add_argument("--space", type=str, help='Color space to convert to [hsv, lab, gray, rgb]')
+    parser.add_argument("--space", type=str, default="original", help='Color space to convert to [hsv, lab, gray, rgb]')
     parser.add_argument("--scale", type=float, default=1.00, help='Scale size of output')
     parser.add_argument("--show", type=bool, default=False, help='Show output')
     parser.add_argument("--output", type=str, default='output/output_image.jpg', help='Output location of file')
