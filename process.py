c4fd6847e8c1c3dee9ac2ada7dc0ef6d3fabd0fe
wayne1tam <waynevick7226@hotmail.com>
2021-03-13 00:02:59 -0500

    Update process with gradient arg

--- process.py
+++ process.py
@@ -1,13 +1,17 @@
+import os.path
+from os import path
 import cv2 as cv
 import argparse
+from gradients import compute_gradient
 from spaces import color_space_convert
 
 def parser():
     parser = argparse.ArgumentParser(description='OpenCV Reader')
-    parser.add_argument("--image", type=str, help='Path to the image')
+    parser.add_argument("--image", type=str, required=True, help='Path to the image')
     parser.add_argument("--space", type=str, default="original", help='Color space to convert to [hsv, lab, gray, rgb]')
+    parser.add_argument("--gradient", type=str, help='Show Image Gradient [laplacian, sobelx, sobely, sobelxy, canny]')
     parser.add_argument("--scale", type=float, default=1.00, help='Scale size of output')
-    parser.add_argument("--show", type=bool, default=False, help='Show output')
+    parser.add_argument("--show", type=bool, default=True, help='Show output')
     parser.add_argument("--output", type=str, default='output/output_image.jpg', help='Output location of file')
 
     return parser.parse_args()
@@ -15,7 +19,14 @@ def parser():
 if __name__=='__main__':
     args = parser()
     image_path = args.image
-    image=cv.imread(image_path)
-    conversion=args.space
-    output=args.output
-    color_space_convert(image, conversion, show=args.show)
+    if path.exists(image_path):
+        image=cv.imread(image_path)
+        if args.space:
+            color_space_convert(image, args.space, show=args.show)
+        
+        if args.gradient:
+            compute_gradient(image, args.gradient, show=args.show)
+
+        output=args.output
+    else:
+        print('Invalid File Path')
