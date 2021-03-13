5e52ee77b86dd3806ef49a384dff219bb0c917a9
wayne1tam <waynevick7226@hotmail.com>
2021-03-12 23:47:20 -0500

    Combine gradient methods to selection

--- gradients.py
+++ gradients.py
@@ -3,37 +3,39 @@ import numpy as np
 from bitwise import bitwise_or
 from spaces import color_space_convert
 
-def laplacian_edge(gray_image, show=False):
-    lap = cv.Laplacian(gray_image, cv.CV_64F)
-    output = np.uint8(np.absolute(lap))
-    if show:
-        cv.imshow('Laplacian Edge', output)
-        cv.waitKey(0)
-    return output
+def compute_gradient(img, gradient, show=False):
+    
+    def laplacian_edge(gray_image, show=False):
+        lap = cv.Laplacian(gray_image, cv.CV_64F)
+        output = np.uint8(np.absolute(lap))
+        return output
 
-def sobel_x(gray_image, show=False):
-    output = cv.Sobel(gray_image, cv.CV_64F, 1, 0)
-    if show:
-        cv.imshow('Sobel X', output)
-        cv.waitKey(0)
-    return output
+    def sobel_x(gray_image):
+        output = cv.Sobel(gray_image, cv.CV_64F, 1, 0)
+        return output
 
-def sobel_y(gray_image, show=False):
-    output = cv.Sobel(gray_image, cv.CV_64F, 0, 1)
-    if show:
-        cv.imshow('Sobel X', output)
-        cv.waitKey(0)
-    return output
+    def sobel_y(gray_image):
+        output = cv.Sobel(gray_image, cv.CV_64F, 0, 1)        
+        return output
 
-def canny_edge(gray_image, show=False):
-    output = cv.Canny(gray_image, 150, 175)
+    def canny_edge(gray_image):
+        output = cv.Canny(gray_image, 150, 175)
+        return output
+    
+    options={
+        'lapacian': laplacian_edge(img),
+        'sobelx': sobel_x(img),
+        'sobely': sobel_y(img),
+        'canny': canny_edge(img)
+    }
+    output=options[gradient]
     if show:
-        cv.imshow('Canny Edge', output)
+        cv.imshow('Gradient Image', output)
         cv.waitKey(0)
     return output
 
 if __name__=='__main__':
-    image_file = 'assets/images/park.jpg'
+    image_file = 'assets/images/nature6.jfif'
     img = cv.imread(image_file)
     gray = color_space_convert(img, 'gray')
-    canny_edge(gray, show=True)
+    compute_gradient(gray, 'canny',show=True)
