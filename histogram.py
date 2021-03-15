1c81e582cddf826175c41e90c877eb092b6dfdc6
wayne1tam <waynevick7226@hotmail.com>
2021-03-15 16:02:58 -0400

    Add argument to display image histogram

--- histogram.py
+++ histogram.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 from spaces import color_space_convert
 from bitwise import bitwise_and
 
-def gray_histogram(image, img_mask=None, show=False):
-    hist = cv.calcHist([image], [0], img_mask, [256], [0,256])
+def show_histogram(image, channel, img_mask=None):
 
-    if show:
+    def gray_histogram():
+        gray_image = (image if len(image.shape) < 3 else color_space_convert(image, 'gray'))
+        hist = cv.calcHist([gray_image], [0], img_mask, [256], [0,256])
+
         plt.figure()
         plt.title('Grayscale Histogram')
         plt.xlabel('Bins')
@@ -16,33 +18,35 @@ def gray_histogram(image, img_mask=None, show=False):
         plt.xlim([0,256])
         plt.show()
 
-    return hist
-
-def color_histogram(image, img_mask=None, show=False):
-    plt.figure()
-    plt.title('Colored Histogram')
-    plt.xlabel('Bins')
-    plt.ylabel('Number of pixels')
-    colors = ('b', 'g', 'r')
-    for i, col in enumerate(colors):
-        hist = cv.calcHist([img], [i], img_mask, [256], [0,256])
-        plt.plot(hist, color=col)
-        plt.xlim(0,256)
-    if show:
+    def color_histogram():
+        plt.figure()
+        plt.title('Colored Histogram')
+        plt.xlabel('Bins')
+        plt.ylabel('Number of pixels')
+        colors = ('b', 'g', 'r')
+        for i, col in enumerate(colors):
+            hist = cv.calcHist([image], [i], img_mask, [256], [0,256])
+            plt.plot(hist, color=col)
+            plt.xlim(0,256)
         plt.show()
-    
-def circle_mask(image, radius, color=255, thickness=-1, show=False):
-    blank = np.zeros(image.shape[:2], dtype='uint8')
-    output = cv.circle(blank, (image.shape[1]//2, image.shape[0]//2), radius, color, thickness)
-    if show:
-        cv.imshow('Circle Mask', output)
-        cv.waitKey(0)
-    return output
+        
+    def circle_mask(radius, color=255, thickness=-1, show=False):
+        blank = np.zeros(image.shape[:2], dtype='uint8')
+        output = cv.circle(blank, (image.shape[1]//2, image.shape[0]//2), radius, color, thickness)
+        if show:
+            cv.imshow('Circle Mask', output)
+            cv.waitKey(0)
+        return output
+    # circle = circle_mask(img, 100, show=True)
+    # bitwise_and(gray, circle)
+
+    if channel=='color':
+        color_histogram()
+    if channel=='gray':
+        gray_histogram()
 
 if __name__=='__main__':
     image_file = 'assets/images/cats.jpg'
     img = cv.imread(image_file)
     gray = color_space_convert(img, 'gray')
-    circle = circle_mask(img, 100)
-    bitwise_and(gray, circle, show=True)
-    color_histogram(img, circle, show=True)
+    show_histogram(img, 'color')
--- process.py
+++ process.py
@@ -6,6 +6,7 @@ from gradients import compute_gradient
 from spaces import color_space_convert
 from rescale import rescale_frame
 from color_channel import display_color_channel
+from histogram import show_histogram
 
 def parser():
     parser = argparse.ArgumentParser(description='OpenCV Reader')
@@ -13,7 +14,8 @@ def parser():
     parser.add_argument("--space", type=str, default="original", help='Color space to convert to [hsv, lab, gray, rgb]')
     parser.add_argument("--gradient", type=str, help='Show Image Gradient [laplacian, sobelx, sobely, sobelxy, canny]')
     parser.add_argument("--channel", type=str, help='Display color channel [blue, green, red]')
-    parser.add_argument("--scale", type=float, default=1.00, help='Scale size of output')
+    parser.add_argument("--histogram", type=str, help='Display image histogram [gray, color]')
+    parser.add_argument("--scale", type=float, help='Scale size of output')
     parser.add_argument("--show", type=bool, default=True, help='Show output')
     parser.add_argument("--output", type=str, default='output/output_image.jpg', help='Output location of file')
 
@@ -31,6 +33,8 @@ if __name__=='__main__':
             gray_image = (image if len(image.shape) < 3 else color_space_convert(image, 'gray'))
             compute_gradient(gray_image, args.gradient, show=args.show)
         if args.channel: display_color_channel(image, args.channel, show=args.show)
+        if args.histogram: 
+            show_histogram(image, args.histogram)
 
         output=args.output
     else:
