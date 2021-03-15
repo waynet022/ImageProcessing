7d802496c1016d8a367e90b9c68de206b9a370d0
wayne1tam <waynevick7226@hotmail.com>
2021-03-15 18:47:14 -0400

    Combine thresholding functions

--- thresh.py
+++ thresh.py
@@ -1,31 +1,34 @@
 import cv2 as cv
 from spaces import color_space_convert
 
-def threshold_img(gray_image, th1, th2, show=False):
-    threshold, thresh = cv.threshold(gray_image, th1, th2, cv.THRESH_BINARY)
-    if show:
-        cv.imshow('Threshold Image', thresh)
-        cv.waitKey(0)
-    return threshold, thresh
+def threshold_img(gray_image, th_op, th1=90, th2=255, show=False):
+    def threshold():
+        threshold, thresh = cv.threshold(gray_image, th1, th2, cv.THRESH_BINARY)
+        return threshold, thresh
+        
+    def invert_threshold():
+        threshold, inv_thresh = cv.threshold(gray_image, th1, th2, cv.THRESH_BINARY_INV)
+        return threshold, inv_thresh
+        
+    def adaptive_threshold(): return cv.adaptiveThreshold(gray_image, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 3)
+        
+    options={
+        'binary': threshold,
+        'inverted': invert_threshold,
+    }
 
-def invert_threshold_img(gray_image, th1, th2, show=False):
-    threshold, inv_thresh = cv.threshold(gray_image, th1, th2, cv.THRESH_BINARY_INV)
-    if show:
-        cv.imshow('Inverted Threshold Image', inv_thresh)
-        cv.waitKey(0)
-    return threshold, inv_thresh
-
-def adaptive_threshold_img(gray_image, show=False):
-    output = cv.adaptiveThreshold(gray_image, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 3)
-    if show:
-        cv.imshow('Adaptive Thresholding', output)
-        cv.waitKey(0)
+    if th_op == 'adaptive': output = adaptive_threshold()
+    else: _, output = options[th_op]()
+    
+    if show: cv.imshow(' threshold output', output) or cv.waitKey(0)
+    
     return output
 
 if __name__=='__main__':
     image_file = 'assets/images/cats.jpg'
     img = cv.imread(image_file)
     gray = color_space_convert(img, 'gray', show=True)
-    threshold_img(gray, 90, 255, show=True)
-    invert_threshold_img(gray, 150, 255, show=True)
-    adaptive_threshold_img(gray, show=True)
+    
+    threshold_img(gray, 'adaptive', show=True)
+    threshold_img(gray, 'binary', 90, 255, show=True)
+    threshold_img(gray, 'inverted', 150, 255, show=True)
